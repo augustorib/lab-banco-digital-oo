c26fd653189e38f1f294c8e5907527097f2a845a
augustorib <augustorifra@gmail.com>
2024-07-17 01:21:42 -0300

    feat: listar contas do banco

--- src/Banco.java
+++ src/Banco.java
@@ -1,9 +1,10 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Banco {
 
 	private String nome;
-	private List<Conta> contas;
+	private List<Conta> contas = new ArrayList<Conta>();
 
 	public String getNome() {
 		return nome;
@@ -21,4 +22,17 @@ public class Banco {
 		this.contas = contas;
 	}
 
+	public void criarContaNoBanco(Conta conta){this.contas.add(conta);}
+
+	public void listarContas()
+	{
+		System.out.println("==== Contas existentes no Banco =======");
+		for(Conta conta : this.contas )
+		{
+			System.out.println("Dono da Conta: " + conta.getCliente().getNome());
+			System.out.println("Agência: " + conta.getAgencia());
+			System.out.println("Número da conta: " + conta.getNumero());
+			System.out.println("------------------------------------------");
+		}
+	}
 }
